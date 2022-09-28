a1640578f023d3c16ac84b9cf041e306ff59816f
Cedric225-dev <81379749+Cedric225-dev@users.noreply.github.com>
2022-09-28 10:26:27 -0400

    Tweaked older-younger check
    Went from 20 to 12 year difference.

--- learn/matcher.ts
+++ learn/matcher.ts
@@ -698,10 +698,10 @@ export class Matcher {
             const youngerCharactersScore = Matcher.getKinkPreference(you, Kink.YoungerCharacters);
             const ageDifference = Math.abs(yourAge - theirAge);
 
-            if ((yourAge < theirAge) && (olderCharactersScore !== null) && (ageDifference >= 20))
+            if ((yourAge < theirAge) && (olderCharactersScore !== null) && (ageDifference >= 12))
                 return Matcher.formatKinkScore(olderCharactersScore, 'older characters');
 
-            if ((yourAge > theirAge) && (youngerCharactersScore !== null) && (ageDifference >= 20))
+            if ((yourAge > theirAge) && (youngerCharactersScore !== null) && (ageDifference >= 12))
                 return Matcher.formatKinkScore(youngerCharactersScore, 'younger characters');
         }
 
