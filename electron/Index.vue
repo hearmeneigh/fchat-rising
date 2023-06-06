50b9921064d4b899ee0b1fd7e55014787c4a109a
Mr. Stallion <mrstallion@nobody.nowhere.fauxdomain.ext>
2023-06-06 14:42:14 -0700

    skip lazy loading for now

--- electron/Index.vue
+++ electron/Index.vue
@@ -150,16 +150,16 @@
     // import Connection from '../fchat/connection';
     // import Notifications from './notifications';
 
-    import VueLazyload from 'vue-lazyload';
-
-    Vue.use(VueLazyload, {
-      observer: true,
-
-      observerOptions: {
-        rootMargin: '0px',
-        threshold: 0,
-      }
-    });
+    // import VueLazyload from 'vue-lazyload';
+    //
+    // Vue.use(VueLazyload, {
+    //   observer: true,
+    //
+    //   observerOptions: {
+    //     rootMargin: '0px',
+    //     threshold: 0,
+    //   }
+    // });
 
     const webContents = remote.getCurrentWebContents();
     const parent = remote.getCurrentWindow().webContents;
